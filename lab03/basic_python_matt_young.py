97a1163ce784c494d2558ff59046efbb162977ff
Matt Young <lorkhb@gmail.com>
2024-02-04 14:45:16 -0700

    Began lab 3.

---
+++ lab03/basic_python_matt_young.py
@@ -0,0 +1,31 @@
+#This is a comment
+
+print("Hello World!")
+
+print("Hello Class!")
+
+-17%3
+
+a = 18
+b = 27
+
+print(a + b)
+
+M = -7
+m = 14
+print(M)
+print(m)
+
+def add_two_numbers(a, b):
+  return a + b
+
+print(add_two_numbers(77, 89))
+
+name = "Harold"
+
+def say_hello():
+  name = "William"
+  print(name)
+
+say_hello()
+print(name)
