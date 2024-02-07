2f4e8e843cccea381e573dab486ca00dcab73ca5
Matt Young <lorkhb@gmail.com>
2024-02-07 12:25:14 -0700

    Lab 4 first phase, testing http requests.

--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -1,7 +1,16 @@
 from django.views import View
 from django.shortcuts import render, redirect
 
+def title_name(the_name):
+    return the_name.casefold()
+
 class HomePageView(View):
     def get(self, request):
-     
-        return render(request, 'my_app/index.html',{'hi':'hello world!'})
+        
+        my_name = "MATTHEW"
+
+        new_name = title_name(my_name)
+
+        context = {'hi':title_name('salutations earthling '), 'name':new_name}
+
+        return render(request, 'my_app/index.html', context)
